2882fa71ee7d6878d0c117a40f12d719a6ff8fab
tomerbuzaglo <tomer.bu@gmail.com>
2016-03-09 13:22:43 +0200

    changed to an enum

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,15 +8,45 @@
 
 import UIKit
 
+enum Phone: CustomStringConvertible{
+    case Home(String)
+    case Office(String)
+    case Mobile(String)
+    
+    var description:String{
+        switch self{
+        case .Mobile(let number):
+            return "Mobile: \(number)"
+        case .Office(let number):
+            return "Office: \(number)"
+        case .Home(let number):
+            return "Home: \(number)"
+        }
+    }
+    init?(rawValue: Int, number:String) {
+        // Check if string contains 'carrousel'
+        switch rawValue{
+        case 0:
+            self = .Home(number)
+        case 1:
+            self = .Office(number)
+        case 2:
+            self = .Mobile(number)
+        default:
+            return nil
+        }
+    }
+}
+
 class ViewController: UIViewController {
-    var arr = [String]()
+    var arr = [Phone]()
     
     @IBAction func saveBtn(sender: UIButton) {
         
         
         if let number = phoneText.text,
-            let type = phoneType.titleForSegmentAtIndex(phoneType.selectedSegmentIndex){
-                arr.append(type + number)
+            let _ = phoneType.titleForSegmentAtIndex(phoneType.selectedSegmentIndex) where phoneType.selectedSegmentIndex >= 0 && phoneType.selectedSegmentIndex <= 2{
+                arr.append(Phone(rawValue: phoneType.selectedSegmentIndex, number:number)!)
                 phoneText.text = ""
         }
         for n in arr{
